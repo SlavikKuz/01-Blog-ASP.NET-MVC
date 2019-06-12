d464e2c056cdb08990f304f0db3c565dbcd9e923
Slavik <slavik.kuz.mail@gmail.com>
2019-06-12 13:02:05 +0200

    defauld database, +SQL BD, +DataLibrary

---
+++ ASPBlog2/Models/UsersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPBlog2.Models
+{
+    public class UsersModel
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string confirmEmail { get; set; }
+        public string passport { get; set; }
+        public string confirmPassport { get; set; }
+
+    }
+}
