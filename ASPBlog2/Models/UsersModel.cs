327389f9a0aac1ec648436366507fa3762d940be
Slavik <slavik.kuz.mail@gmail.com>
2019-06-12 15:16:52 +0200

    UserModel added, User create view

--- ASPBlog2/Models/UsersModel.cs
+++ ASPBlog2/Models/UsersModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,40 @@ namespace ASPBlog2.Models
 {
     public class UsersModel
     {
+        [Display(Name="Age")]
+        [Range(7,70, ErrorMessage ="Please, enter the age from 7 to 70 years.")]
         public int age { get; set; }
-        public string name { get; set; }
-        public string lastname { get; set; }
-        public string email { get; set; }
-        public string confirmEmail { get; set; }
-        public string passport { get; set; }
-        public string confirmPassport { get; set; }
 
+        [Display(Name = "Name")]
+        [Required(ErrorMessage ="Please fill in the field Name.")]
+        public string name { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please fill in the field Last Name.")]
+        public string lastname { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Please fill in the field Email.")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "The Email must be from 4 to 50 symbols.")]
+        public string email { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Confirm Email")]
+        [StringLength(50, MinimumLength = 4, ErrorMessage = "The Email must be from 4 to 50 symbols.")]
+        [Required(ErrorMessage = "Please fill in the field Confirm Email.")]
+        [Compare("Email", ErrorMessage ="Email confirm is wrong!")]
+        public string confirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please fill in the field Password.")]
+        [StringLength(50, MinimumLength =4, ErrorMessage ="The password must be from 4 to 50 symbols.")]
+        public string passport { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password confirm is wrong!")]
+        public string confirmPassport { get; set; }
     }
 }
